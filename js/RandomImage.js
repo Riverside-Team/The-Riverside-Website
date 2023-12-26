a7de61a589cf73dcc86e3eec341293388457cc0b
sso <93847254+ssoq@users.noreply.github.com>
2023-12-26 00:33:16 +0000

    Keycard Images for random image switcher

--- js/RandomImage.js
+++ js/RandomImage.js
@@ -1,16 +1,48 @@
-const imageLocations = ["img/Images_For_Random/black_logo.png"]
+const imageLocations = 
+[
+    "img/white_logo.png",
+    "img/Images_For_Random/RENDER1.png",
+    "img/Images_For_Random/RENDER2.png",
+    "img/Images_For_Random/RENDER3.png",
+    "img/Images_For_Random/RENDER4.png",
+    "img/Images_For_Random/RENDER5.png",
+    "img/Images_For_Random/RENDER6.png",
+    "img/Images_For_Random/RENDER7.png",
+    "img/Images_For_Random/RENDERX.png"
+]
 
 function ChangeImageRandomly() 
 {
-    let randomFlip = Math.floor(Math.random() * 2);
+    let randomFlip = Math.floor(Math.random() * imageLocations.length + 1);
     var image = document.getElementById('image');
     switch (randomFlip) 
     {
-        case 0:
-            image.src = "img/black_logo.png";
-            break;
         case 1:
-            image.src = "img/white_logo.png";
+            image.src = imageLocations[0];
+            break;
+        case 2:
+            image.src = imageLocations[1];
+            break;
+        case 3:
+            image.src = imageLocations[2];
+            break;
+        case 4:
+            image.src = imageLocations[3];
+            break;
+        case 5:
+            image.src = imageLocations[4];
+            break;
+        case 6:
+            image.src = imageLocations[5];
+            break;
+        case 7:
+            image.src = imageLocations[6];
+            break;
+        case 8:
+            image.src = imageLocations[7];
+            break;
+        case 9:
+            image.src = imageLocations[8];
             break;
     }
 }
